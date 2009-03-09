7bc2e9d68bdaa5e2cc0bae3018cd399ee2fe4930
Ian McKellar <yakk@glub.dreamhost.com>
2009-03-08 18:51:42 -0700

    added favicon

--- wp-content/themes/thematic/header.php
+++ wp-content/themes/thematic/header.php
@@ -22,7 +22,8 @@
   	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php echo wp_specialchars(get_bloginfo('name'), 1) ?> <?php _e('Posts RSS feed', 'thematic'); ?>" />
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php echo wp_specialchars(get_bloginfo('name'), 1) ?> <?php _e('Comments RSS feed', 'thematic'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
-	
+  <link rel="icon" href="/favicon.png" type="image/png" />
+
 <?php wp_head() ?>
 
 </head>
